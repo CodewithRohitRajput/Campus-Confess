d2dc20c57a56e2f235f2a7cb5ed64301f5d19d58
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2026-01-05 23:09:12 +0530

    add cluster module for scalability

--- server/config/redis.ts
+++ server/config/redis.ts
@@ -7,17 +7,14 @@ export const  connectRedis = async () =>{
         redisClient = createClient({
             url : process.env.REDIS_URL || 'redis://localhost:6379'
         })
-        if(!process.env.REDIS_URL){
-            console.log('redis is not connected env is not present');
-            redisClient = null;
-            return;
-        }
+        
         redisClient.on("error" , (err)=>{
             console.error('redis facing issues...' , err);
         })
         await redisClient.connect();
         console.log("Redis Connected Successfully");
     }catch(err){
+        console.error('Failed to connect to Redis:', err);
         redisClient = null;
     }
 }
--- server/server.ts
+++ server/server.ts
@@ -4,23 +4,40 @@ import connectDB from "./config/db.js";
 import collegeRouter from "./routes/collegeRoute.js"
 import confessionRouter from "./routes/confessionRoute.js"
 import { connectRedis } from "./config/redis.js";
-const app = express();
+import cluster from "cluster";
+import os from 'os'
+import process from "process";
 
-app.use(express.json());
-app.use(cors(
+
+const cpuCores = os.cpus().length;
+console.log('cpu',cpuCores)
+
+if(cluster.isPrimary){
+    for(let i=0; i<cpuCores;i++){
+        cluster.fork();
+    }
+}else{
+
+    
+    const app = express();
+    
+    app.use(express.json());
+    app.use(cors(
         {
             origin : ["https://campus-confess.vercel.app" , "http://localhost:3000"],
             credentials : true
         }
-));
-
-await connectDB();
-await connectRedis();
-
-
-app.use('/college' , collegeRouter);
-app.use('/confession' , confessionRouter);
-
-app.listen(8000,()=>{
-    console.log(`server is running on port ${8000}`);
-})
+    ));
+    
+    await connectDB();
+    await connectRedis();
+    
+    
+    app.use('/college' , collegeRouter);
+    app.use('/confession' , confessionRouter);
+    
+    app.listen(8000,()=>{
+        console.log(`server is running on port ${8000}`);
+    })
+    
+}
